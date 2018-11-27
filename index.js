39108175022cc82d9035c8e2988dd73a612b5d36
OSCAR RIOS <oscr.rss@gmail.com>
2018-11-27 05:46:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,14 +9,30 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(item) {
- // write your code here
+function addToCart(item){
+  var price = Math.floor(100*(Math.random())) % 100 + 1;
+  var itemObj = { 
+                  itemName: item,
+                  itemPrice: price
+                };
+  cart.push(itemObj);
+  return (`${itemObj.itemName} has been added to your cart.`);
 }
 
 function viewCart() {
-  // write your code here
+    if(cart.length === 1){
+      return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
+    }else if (cart.length > 1){
+      let string = `In your cart, you have `
+      for (let i = 0; i < cart.length; i++){
+        string += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+        if(i === cart.length - 1){
+          return string += `and ${cart[i].itemName} at $${cart[i].itemPrice}.`}}
+    }
+    else{
+      return `Your shopping cart is empty.`
+  }
 }
-
 function total() {
   // write your code here
 }
